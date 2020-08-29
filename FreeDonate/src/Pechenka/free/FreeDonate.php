865399b85f0785ba09ce177262a2d4140b2bc956
PechenkaPM <47981825+PechenkaPM@users.noreply.github.com>
2020-08-30 00:10:25 +0300

    Сохранение данных при выключении сервера

--- FreeDonate/src/Pechenka/free/FreeDonate.php
+++ FreeDonate/src/Pechenka/free/FreeDonate.php
@@ -49,6 +49,11 @@ class FreeDonate extends PluginBase
      */
     public function onDisable()
     {
+        foreach (EventListener::$players as $name => $time) {
+            $data = self::$data->get($name);
+            $data += time() - $time;
+            self::$data->set($name, $data);
+        }
         self::$data->save();
     }
     
